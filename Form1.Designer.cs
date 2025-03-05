eaf667da39acff5f1cb2a744f4722d963af89fdc
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-05 17:13:24 +0700

    menambahkan implementasi kalkulator

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnSamaDengan = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(199, 162);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 39);
+            this.btnPlus.TabIndex = 0;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSamaDengan
+            // 
+            this.btnSamaDengan.Location = new System.Drawing.Point(280, 162);
+            this.btnSamaDengan.Name = "btnSamaDengan";
+            this.btnSamaDengan.Size = new System.Drawing.Size(75, 39);
+            this.btnSamaDengan.TabIndex = 1;
+            this.btnSamaDengan.Text = "=";
+            this.btnSamaDengan.UseVisualStyleBackColor = true;
+            this.btnSamaDengan.Click += new System.EventHandler(this.btnSamaDengan_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(361, 162);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(75, 39);
+            this.btn0.TabIndex = 2;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(199, 207);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 39);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(280, 207);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 39);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(361, 207);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 39);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(199, 252);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 39);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(280, 252);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 39);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(361, 252);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 39);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(199, 297);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 39);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(280, 297);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 39);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(361, 297);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 39);
+            this.btn9.TabIndex = 11;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(196, 115);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 13);
+            this.labelOutput.TabIndex = 12;
+            this.labelOutput.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnSamaDengan);
+            this.Controls.Add(this.btnPlus);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnSamaDengan;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label labelOutput;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,186 @@ namespace modul3_103022300048
 {
     public partial class Form1 : Form
     {
+        int angka = 0, summ = 0;
+        bool duaDigit = false;
+        string clickTambah = "";
+        string result = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            summ += angka;
+            clickTambah += angka;
+            labelOutput.Text = clickTambah;
+            angka = 0;
+            duaDigit = false;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            summ += angka;
+            clickTambah += angka;
+            labelOutput.Text = clickTambah;
+            result += "" + summ;
+            labelOutput.Text = result;
+            summ = 0;
+            angka = 0;
+            duaDigit = false;
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 0;
+                duaDigit = true;
+            }
+            else
+            {
+                angka = (angka * 10);
+            }
+        }
+
+        private void btnSamaDengan_Click(object sender, EventArgs e)
+        {
+            
+           
+            {
+
+            }
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 1;
+                duaDigit = true;
+            } else
+            {
+                angka = (angka * 10) + 1;   
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 2;
+                duaDigit = true;
+            }
+            else
+            {
+                angka = (angka * 10) + 2;
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 3;
+                duaDigit = true;
+            }
+            else
+            {
+                angka = (angka * 10) + 3;
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 4;
+                duaDigit = true;
+            }
+            else
+            {
+                angka = (angka * 10) + 4;
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 5;
+                duaDigit = true;
+            }
+            else
+            {
+                angka = (angka * 10) + 5;
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 6;
+                duaDigit = true;
+            }
+            else
+            {
+                angka = (angka * 10) + 6;
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 7;
+                duaDigit = true;
+            }
+            else
+            {
+                angka = (angka * 10) + 7;
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 8;
+                duaDigit = true;
+            }
+            else
+            {
+                angka = (angka * 10) + 8;
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (!duaDigit)
+            {
+                angka = 9;
+                duaDigit = true;
+            }
+            else
+            {
+                angka = (angka * 10) + 9;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
